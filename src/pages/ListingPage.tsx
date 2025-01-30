5f1cf488c21d565f76caae88d6b7a4f78f637c42
samuelgbenga <samuelgbenga972@gmail.com>
2025-01-30 04:09:08 +0100

    completed the sub Hero

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from '../sections/heroSection/HeroSection'
 import CauroselSection from '../sections/caurosel/CauroselSection'
+import SubHeroSection from '../sections/subHero/SubHeroSection'
 
 const ListingPage = () => {
   return (
     <div><HeroSection />
     <CauroselSection/>
+    <SubHeroSection/>
     </div>
     
   )
---
+++ src/sections/subHero/SubHeroSection.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import style from "./subHeroSection.module.css";
+import LeftHeroSub from "../../assets/LeftHeroSub";
+import Button from "../../components/botton/Button";
+import can from "../../assets/images/can.png";
+import candy from "../../assets/images/candy.png";
+
+const SubHeroSection = () => {
+  return (
+    <div className={style.subHeroSection}>
+      <div className={style.innerContainer}>
+        <div className={style.left}>
+          <LeftHeroSub />
+          <div className={style.leftBtns}>
+            <Button variant="secondary" label="Get Started" />
+            <Button variant="primary" label="Learn more" />
+          </div>
+        </div>
+        <div className={style.right}>
+          <div className={style.rightTop}>
+          <img src={candy} alt="candy" />
+           
+          </div>
+          <div className={style.rightBottom}>
+          <img src={can} alt="can" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubHeroSection;
