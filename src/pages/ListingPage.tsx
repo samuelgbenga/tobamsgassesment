d421edec209e14907fa4ec46443b97ed6b42566d
samuelgbenga <samuelgbenga972@gmail.com>
2025-01-30 05:03:39 +0100

    second hero section

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeroSection from '../sections/heroSection/HeroSection'
 import CauroselSection from '../sections/caurosel/CauroselSection'
 import SubHeroSection from '../sections/subHero/SubHeroSection'
+import SecondSubHero from '../sections/secondSubHero/SecondSubHero'
 
 const ListingPage = () => {
   return (
     <div><HeroSection />
     <CauroselSection/>
     <SubHeroSection/>
+    <SecondSubHero/>
     </div>
     
   )
---
+++ src/sections/secondSubHero/SecondSubHero.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import style from "./secondSubHero.module.css";
+import SecondSubHeroLeft from "../../assets/SecondSubHeroLeft";
+import SecondSubHeroIcon from "../../assets/SecondSubHeroIcon";
+import Button from "../../components/botton/Button";
+import cloud from "../../assets/images/cloud.png";
+import emoji from "../../assets/images/emoji.png";
+
+const SecondSubHero = () => {
+  return (
+    <div className={style.secondSubHero}>
+      <div className={style.innerContainer}>
+        <div className={style.left}>
+
+        <div className={style.leftTop}>
+          <img src={cloud} alt="candy" />
+           
+          </div>
+          <div className={style.leftBottom}>
+          <img src={emoji} alt="can" />
+          </div>
+
+        </div>
+        <div className={style.right}>
+          <div>
+            <SecondSubHeroLeft />
+          </div>
+          <div>
+            <SecondSubHeroIcon />
+          </div>
+          <div>
+            <Button variant="secondary" label="Get Started" />
+            <Button variant="primary" label="Learn more" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondSubHero;
